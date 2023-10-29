3e845d383d0b69c5c6c9a91fdf8101d4785d4741
rkrk0285 <82379491+rkrk0285@users.noreply.github.com>
2023-10-29 15:17:43 +0900

    플레이어 이동 구현
    -

---
+++ Swarm_Forest/Assets/Script/Player/InputManager.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InputManager : MonoBehaviour
+{
+    [SerializeField]
+    GameObject Player;
+    [SerializeField]
+    Plane _plane;
+    
+    void Update()
+    {
+        input_Control();
+    }
+
+    void input_Control()
+    {
+        // 오른쪽 클릭 입력
+        if (Input.GetMouseButton(1))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                Player.GetComponent<PlayerManager>().movePlayer(hit.point);
+            }
+        }
+
+        if (Input.GetKeyDown(KeyCode.Q))
+        {
+            Debug.Log("Q 입력");
+        }
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            Debug.Log("W 입력");
+        }
+        if (Input.GetKeyDown(KeyCode.E))
+        {
+            Debug.Log("E 입력");
+        }
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log("R 입력");
+        }
+    }
+}
