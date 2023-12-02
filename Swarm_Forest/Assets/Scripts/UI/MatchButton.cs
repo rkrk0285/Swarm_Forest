ac2c550eee933cd3bfb2a3486cd5d798a4462bd0
YEGU KWON <wt2080@gmail.com>
2023-12-02 20:14:28 +0900

    Make same indentation as other lines

--- Swarm_Forest/Assets/Scripts/UI/MatchButton.cs
+++ Swarm_Forest/Assets/Scripts/UI/MatchButton.cs
@@ -95,7 +95,7 @@ public class MatchButton : MonoBehaviour
     
     IEnumerator LoadMyAsyncScene()
     {    
-    // AsyncOperation을 통해 Scene Load 정도를 알 수 있다.
+        // AsyncOperation을 통해 Scene Load 정도를 알 수 있다.
         AsyncOperation asyncLoad = SceneManager.LoadSceneAsync("GameScene");
 
         // Scene을 불러오는 것이 완료되면, AsyncOperation은 isDone 상태가 된다.
